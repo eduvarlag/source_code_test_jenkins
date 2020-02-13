40c0a4603adfb647fe8b8a0f414c442c8926ae2b
eduvarlag <eduvarlag@gmail.com>
2020-02-13 04:19:20 -0500

    subimos un cambio para ver si lo pilla el jenkins

--- test.sh
+++ test.sh
@@ -3,4 +3,5 @@ hostname
 echo "Hola soy Edu"
 echo $TESTVAR
 echo "VEEEENGA QUE HEMOS PUESTO UN HOOK PARA PILLAR LOS CAMBIOS"
-echo "jenkins2"
+echo "jenkins"
+echo "PROBAMOS CON EL TEST DE LINUXACADEMY"
