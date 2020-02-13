e113e47e32ba9042cc7b338e9b22179bc71f3bbf
eduvarlag <eduvarlag@gmail.com>
2020-02-13 10:34:18 +0000

    a ver ahora

--- test.sh
+++ test.sh
@@ -1,8 +1,4 @@
 uname -a
 hostname
-echo "Hola soy Edu"
 echo $TESTVAR
-echo "VEEEENGA QUE HEMOS PUESTO UN HOOK PARA PILLAR LOS CAMBIOS"
-echo "jenkins"
-echo "PROBAMOS CON EL TEST DE LINUXACADEMY"
-echo "estamos utilzando ngrok"
+echo "PRUEBACA CON MI JENKINS" 
